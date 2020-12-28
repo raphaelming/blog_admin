8801e83827318dd5d9dfc252db06ba50f9194e86
raphaelming <spear131121@gmail.com>
2020-12-28 19:25:59 +0900

    project init

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # blog_admin
+
 tech blog admin
---
+++ src/test/java/com/raphelming/blog_admin/BlogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raphelming.blog_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
